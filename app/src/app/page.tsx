3f984addff7d067ad8483941713c424810f45c75
evankins <112184136+evankins@users.noreply.github.com>
2024-11-29 10:35:33 -0500

    text and simple tailwind css in

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,7 +1,54 @@
+import { NavigationMenuDemo } from "@/app/NavigationMenuDemo"
+import './globals.css'
+
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-5xl">Evan Kinsey</h1>
+    <div className="flex flex-col justify-center items-center"> 
+      <div className="w-11/12 md:w-7/12">   
+        <NavigationMenuDemo />
+
+        <h1 className="">Welcome!</h1>
+
+        <p className="my-4">
+          Hello 👋🏻 My name is <b>Evan Kinsey</b>!
+        </p>
+
+        <p className="my-4">
+          I&apos;m a...
+        </p>
+
+        <p className="my-2 mt-8">
+          Third-year student at RIT studying...
+        </p>
+
+        <h2 className="mb-12">
+          Software Engineering
+        </h2>
+
+        <p>
+          A Google certified...
+        </p>
+
+        <h2 className="mb-12">
+          Data Analyst
+        </h2>
+
+        <p>
+          I specialize in making the complex simple. Whether it&apos;s making <b>robust applications</b> or <b>powerful data visualizations</b>, I prioritize finding elegant solutions.
+        </p>
+
+        <p className="my-8">
+          To read more about me and my work ethic, you can head to my About Me page.
+        </p>
+
+        <p className="my-8">
+          To see my skills and completed projects, you can head to my Experience page.
+        </p>
+
+
+      </div>
     </div>
+
   );
 }
