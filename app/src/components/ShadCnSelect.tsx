8e28107ef50bbac1f8a74690a5459d54a4bcaa1b
evankins <112184136+evankins@users.noreply.github.com>
2024-12-01 19:38:38 -0500

    select by database

--- app/src/components/ShadCnSelect.tsx
+++ app/src/components/ShadCnSelect.tsx
@@ -23,6 +23,7 @@ export function FilterSelect({ onChange }: FilterSelectProps) {
         <SelectGroup>
           <SelectItem value="All" className="hover:bg-slate-200 transition duration-300 rounded-md"><p>All</p></SelectItem>
           <SelectItem value="Language" className="hover:bg-slate-200 transition duration-300 rounded-md"><p>Language</p></SelectItem>
+          <SelectItem value="Database" className="hover:bg-slate-200 transition duration-300 rounded-md"><p>Database</p></SelectItem>
           <SelectItem value="Framework" className="hover:bg-slate-200 transition duration-300 rounded-md"><p>Framework</p></SelectItem>
           <SelectItem value="Other" className="hover:bg-slate-200 transition duration-300 rounded-md"><p>Other</p></SelectItem>
         </SelectGroup>
